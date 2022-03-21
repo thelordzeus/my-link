244c5af69b6e71e3d0c867d6be7e40df181d8598
thelordzeus <arnav55125@gmail.com>
2022-03-21 13:40:59 +0530

    links fixed

--- src/links.js
+++ src/links.js
@@ -2,26 +2,26 @@ const Links = () => {
   return (
     <nav className="socials">
       <div className="socialLinks">
-        <a href="https://github.com/thelordzeus" target="_blank">
+        <a target="_blank" href="https://github.com/thelordzeus">
           <i class="fa-brands fa-github fa-5x"></i>
         </a>{" "}
         &nbsp; &nbsp; &nbsp;
-        <a href="https://www.instagram.com/thelordzeus_/" target="_blank">
+        <a target="_blank" href="https://www.instagram.com/thelordzeus_/">
           <i class="fa-brands fa-instagram fa-5x"></i>
         </a>{" "}
         &nbsp; &nbsp; &nbsp;
-        <a href="https://www.linkedin.com/in/arnavshukla" target="_blank">
+        <a target="_blank" href="https://www.linkedin.com/in/arnavshukla">
           <i class="fa-brands fa-linkedin fa-5x"></i>
         </a>{" "}
         &nbsp; &nbsp; &nbsp;
         <a
-          href="https://open.spotify.com/user/piow1fwb1gcubr7e605b4ayxe?si=ANKNERB-TNywTab4MjXVDw"
           target="_blank"
+          href="https://open.spotify.com/user/piow1fwb1gcubr7e605b4ayxe?si=ANKNERB-TNywTab4MjXVDw"
         >
           <i class="fa-brands fa-spotify fa-5x"></i>
         </a>{" "}
         &nbsp; &nbsp; &nbsp;
-        <a href="https://discordapp.com/users/lordzeus#4525" target="_blank">
+        <a target="_blank" href="https://discordapp.com/users/lordzeus#4525">
           <i class="fa-brands fa-discord fa-5x"></i>
         </a>
       </div>
